8036b6733e76a5dd31d149ea8d154e400c97c3d0
Adam Sidor <sidoadsi1@gmail.com>
2025-01-05 21:36:09 +0100

    Initial Commit

---
+++ CarGame/CarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarGameApp.swift
+//  CarGame
+//
+//  Created by Adam Sidor on 05/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct CarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
