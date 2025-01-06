3c8c0b77f6037970987ecb08d77f70ead0b35492
Adam Sidor <sidoadsi1@gmail.com>
2025-01-06 01:50:00 +0100

    changed 2 arrays to 1 using new class Car

--- CarGame/CarGameApp.swift
+++ CarGame/CarGameApp.swift
@@ -1,5 +1,20 @@
 import SwiftUI
 
+class Car: ObservableObject {
+    @Published var direction: Int
+    @Published var image: Image
+    
+    init(direction: Int) {
+        self.direction = direction
+        self.image = Image("car\(direction)")
+    }
+    
+    func refreshView() {
+        image = Image("car\(direction)")  // Przypisanie nowego obrazu
+    }
+}
+
+
 @main
 struct CarGameApp: App {
     var body: some Scene {
--- CarGame/ContentView.swift
+++ CarGame/ContentView.swift
@@ -2,76 +2,70 @@ import SwiftUI
 
 struct ContentView: View {
     @State var moves: Int = 0
-    
-    @State var carsDirection: [[Int]] = Array(repeating: Array(repeating: 0, count: 6), count: 6)
-    @State var carsImages: [[Image]] = Array(repeating: Array(repeating: Image("car1"), count: 6), count: 6)
-    
-    func refreshCarView(row:Int,column:Int){
-        carsImages[row][column] = Image("car" + String(carsDirection[row][column]))
-    }
-    
+    @State var cars: [[Car]] = [[]]
+
     func changeCarLocation(row:Int,column:Int){
-        if(carsDirection[row][column]==1){
+        if(cars[row][column].direction==1){
             if row>0{
-                if carsDirection[row-1][column] == 0{
-                    carsDirection[row][column]=0;
-                    carsDirection[row-1][column]=1;
-                    refreshCarView(row: row-1, column: column)
+                if cars[row-1][column].direction == 0{
+                    cars[row][column].direction=0
+                    cars[row-1][column].direction=1
+                    cars[row-1][column].refreshView()
                 }
             }
             else{
-                carsDirection[row][column]=0;
+                cars[row][column].direction=0;
             }
             
         }
-        if(carsDirection[row][column]==2){
+        if(cars[row][column].direction==2){
             if column<5{
-                if carsDirection[row][column+1] == 0{
-                    carsDirection[row][column]=0;
-                    carsDirection[row][column+1]=2;
-                    refreshCarView(row: row, column: column+1)
+                if cars[row][column+1].direction == 0{
+                    cars[row][column].direction=0
+                    cars[row][column+1].direction=2
+                    cars[row][column+1].refreshView()
                 }
             }
             else{
-                carsDirection[row][column]=0;
+                cars[row][column].direction=0;
             }
             
         }
-        if(carsDirection[row][column]==3){
+        if(cars[row][column].direction==3){
             if row<5{
-                if carsDirection[row+1][column] == 0{
-                    carsDirection[row][column]=0;
-                    carsDirection[row+1][column]=3;
-                    refreshCarView(row: row+1, column: column)
+                if cars[row+1][column].direction == 0{
+                    cars[row][column].direction=0
+                    cars[row+1][column].direction=3
+                    cars[row+1][column].refreshView()
                 }
             }
             else{
-                carsDirection[row][column]=0;
+                cars[row][column].direction=0;
             }
             
         }
-        if(carsDirection[row][column]==4){
+        if(cars[row][column].direction==4){
             if column>0{
-                if carsDirection[row][column-1] == 0{
-                    carsDirection[row][column]=0;
-                    carsDirection[row][column-1]=4;
-                    refreshCarView(row: row, column: column-1)
+                if cars[row][column-1].direction == 0{
+                    cars[row][column].direction=0
+                    cars[row][column-1].direction=4
+                    cars[row][column-1].refreshView()
                 }
             }
             else{
-                carsDirection[row][column]=0;
+                cars[row][column].direction=0;
             }
             
         }
-        refreshCarView(row: row, column: column)
+        cars[row][column].refreshView()
     }
     
     var body: some View {
         VStack {
-            ForEach(carsImages.indices, id: \.self) { i in
+            ForEach(cars.indices, id: \.self) { i in
                 HStack {
-                    ForEach(carsImages[i].indices, id: \.self) { j in
-                            carsImages[i][j]
+                    ForEach(cars[i].indices, id: \.self) { j in
+                        cars[i][j].image
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .onTapGesture {
@@ -85,14 +79,11 @@ struct ContentView: View {
         }
         .padding()
         .onAppear() {
-            carsDirection = (0..<6).map { _ in
-                        (0..<6).map { _ in
-                            Int.random(in: 0...3)
-                        }
-                    }
-            carsImages = (0..<6).map { i in
-                (0..<6).map { j in
-                    Image("car" + String(carsDirection[i][j]))
+            for i in 0..<6 {
+                cars.append([])
+                for j in 0..<6 {
+                    cars[i].append(Car(direction: Int.random(in: 0..<4)))
+                    cars[i][j].refreshView()
                 }
             }
         }
